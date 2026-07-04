0b9c626348a8af3e8c7b4c7cb71a886d3c5756e6
munasinghe-2006 <methsandimunasinghe7@gmail.com>
2026-07-04 22:39:36 +0530

    "model loaded to the program"

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+from pathlib import Path
+import pickle
 
 app = FastAPI(
     title="FastAPI Template",
     description="This is a sample FastAPI template.",
     version="1.0.0",)
+
+MODEL_PATH = Path(__file__).resolve().parent / "models"/"bengaluru_house_price_linear_regression_model.pickle"
+
+try:
+    with open(MODEL_PATH, "rb") as file:
+        model_package = pickle.load(file)
+
+        model = model_package["model"]
+        feature_columns = model_package["feature_columns"]
+ 
+except FileNotFoundError:
+    raise RuntimeError(f"The pickle file was not found at the specified path: {MODEL_PATH}")
+
+except KeyError:
+    raise RuntimeError("The pickle file does not contain the expected keys: 'model' and 'feature_columns'.")
+
+except Exception as e:
+    raise RuntimeError(f"An unexpected error occurred: {e}")
+
+
 
 @app.get("/")
 def read_root():
