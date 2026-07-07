2453ab4955ab36d8e65e1f4f1d6b62e2dcd59cbc
munasinghe-2006 <methsandimunasinghe7@gmail.com>
2026-07-07 15:44:50 +0530

    "env configure an untracked"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+
 from fastapi import FastAPI
 from pathlib import Path
 import pickle
+from pydantic import BaseModel
+import numpy as np
+
+class HousePricePredictionInput(BaseModel):
+    total_sqft: float
+    bath: int
+    balcony: int
+    bhk: int
+    area_type: str
+    location: str
+
 
 app = FastAPI(
     title="FastAPI Template",
@@ -25,7 +37,42 @@ except KeyError:
 except Exception as e:
     raise RuntimeError(f"An unexpected error occurred: {e}")
 
+NUMERICAL_TYPE_FEATURES = ["total_sqft", "bath", "balcony", "bhk"]
 
+AREA_TYPE_FEATURES = ["Built-up  Area", "Carpet  Area", "Plot  Area", "Super built-up  Area"]
+
+LOCATION_TYPE_FEATURES = [
+    feature for feature in feature_columns if feature not in NUMERICAL_TYPE_FEATURES and feature not in AREA_TYPE_FEATURES
+]
+
+def create_input_row(data: HousePricePredictionInput):
+    input_row = np.zeros(len(feature_columns))
+
+    numerical_features = {
+        "total_sqft": data.total_sqft,
+        "bath": data.bath,
+        "balcony": data.balcony,
+        "bhk": data.bhk
+    }
+
+    for feature, value in numerical_features.items():
+        feature_index = feature_columns.index(feature)
+        input_row[feature_index] = value
+
+    if data.area_type not in AREA_TYPE_FEATURES:
+        return HTTPException(status_code=400, detail=f"Invalid area_type. Must be one of: {AREA_TYPE_FEATURES}")
+    
+    area_type_index = feature_columns.index(data.area_type)
+    input_row[area_type_index] = 1
+
+    if data.location not in LOCATION_TYPE_FEATURES:
+        return HTTPException(status_code=400, detail=f"Invalid location. Must be one of: {LOCATION_TYPE_FEATURES}")
+    
+    location_index = feature_columns.index(data.location)
+    input_row[location_index] = 1
+
+    return input_row
+              
 
 @app.get("/")
 def read_root():
@@ -44,4 +91,27 @@ def read_model_info():
         "sample_feature_columns": feature_columns[:10]
     }
 
+@app.get("/options")
+def read_options():
+    return {
+        "area_types": AREA_TYPE_FEATURES,
+        "location_types": LOCATION_TYPE_FEATURES
+    }
 
+@app.post("/predict")
+def predict(data: HousePricePredictionInput):
+    input_row = create_input_row(data)
+    prediction_result = model.predict([input_row])[0]
+
+    return{
+        "prediction": round(prediction_result, 2),
+           "input_data": {
+               "total_sqft": data.total_sqft,
+               "bath": data.bath,
+               "balcony": data.balcony,
+               "bhk": data.bhk,
+               "area_type": data.area_type,
+               "location": data.location
+           }
+           }
+    
